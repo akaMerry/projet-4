4f79e018bdba608ee42482483e485ea4b42978f3
Merry <aliasmerry@gmail.com>
2025-03-13 12:04:42 +0100

    synchro session de mentorat

--- src/assets/modal.js
+++ src/assets/modal.js
@@ -6,14 +6,17 @@ const form = document.querySelector("form");
 const formComplete = document.querySelector(".form-complete");
 const formData = document.querySelectorAll(".formData");
 const inputs = form.querySelectorAll("input");
+const x = document.getElementById("myTopnav");
+const i = x.querySelector(".icon i");
 
 // Toggle navbar and icon (responsive)
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  var i = document.querySelector(".icon i");
-
+  navbarIcon();
   x.classList.toggle("responsive");
+}
 
+// navbar icon
+function navbarIcon() {
   if (i.classList.contains("fa-bars")) {
     i.classList.remove("fa", "fa-bars");
     i.classList.add("fa-solid", "fa-x");
@@ -26,15 +29,18 @@ function editNav() {
 // Launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  if (x.classList.contains("responsive")) {
+    navbarIcon();
+    x.classList.remove("responsive");
+  }
 }
-
-// Launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // Close modal form
 function closeModal() {
   modalbg.style.display = "none";
 }
+// Launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // Close modal event
 modalClose.forEach((element) => element.addEventListener("click", closeModal));
@@ -42,14 +48,18 @@ modalClose.forEach((element) => element.addEventListener("click", closeModal));
 // Manage custom error messages
 function errorMessage(input) {
   if (input.validity.valueMissing) {
-    if (input.type === "checkbox") {
-      input.setCustomValidity(
-        "Veuillez vérifier que vous acceptez les conditions d'utilisation."
-      );
-    } else if (input.type === "radio") {
-      input.setCustomValidity("Veuillez choisir une option.");
-    } else {
-      input.setCustomValidity("Veuillez renseigner ce champ.");
+    switch (input.type) {
+      case "checkbox":
+        input.setCustomValidity(
+          "Veuillez vérifier que vous acceptez les conditions d'utilisation."
+        );
+        break;
+      case "radio":
+        input.setCustomValidity("Veuillez choisir une option.");
+        break;
+      default:
+        input.setCustomValidity("Veuillez renseigner ce champ.");
+        break;
     }
   } else if (input.validity.tooShort) {
     input.setCustomValidity(
@@ -65,6 +75,24 @@ function errorMessage(input) {
     input.setCustomValidity("La valeur est trop grande.");
   } else if (input.validity.stepMismatch) {
     input.setCustomValidity("La valeur est incorrecte.");
+  } else if (input.validity.badInput) {
+    input.setCustomValidity("La saisie est incorrecte.");
+  } else if (input.validity.typeMismatch) {
+    switch (input.type) {
+      case "email":
+        input.setCustomValidity(
+          "L'adresse e-mail doit contenir les caractères suivants : @ et ."
+        );
+        break;
+      case "url":
+        input.setCustomValidity("L'URL n'est pas valide.");
+        break;
+      default:
+        input.setCustomValidity("Le format est incorrect.");
+        break;
+    }
+  } else if (input.validity.customError) {
+    input.setCustomValidity("Une erreur est survenue.");
   } else {
     input.setCustomValidity("");
   }
